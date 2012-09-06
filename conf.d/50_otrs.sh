8240c2383bfc6cf260e2d5ab760bd1ed420c3da2
Benjamin Heisig <benjamin@heisig.name>
2012-09-06 13:37:16 +0200

    improving module "OTRS"
    *  still incomplete => manual configuration needed
    *  more log messages
    *  more TODO hints

--- conf.d/50_otrs.sh
+++ conf.d/50_otrs.sh
@@ -35,17 +35,24 @@ function do_install {
     loginfo "Installing OTRS Help Desk..."
     cd "$TMP_DIR" || return 1
     download http://ftp.otrs.org/pub/otrs/otrs-3.1.10.tar.bz2 || return 1
+    logdebug "Extracting tarball..."
     tar xjf otrs-3.1.10.tar.bz2 || return 1
-    mv otrs-3.1.9/ /opt/otrs/ || return 1
+    logdebug "Moving files to destination directory..."
+    mv otrs-3.1.10/ /opt/otrs/ || return 1
+    logdebug "Checking modules..."
     perl /opt/otrs/bin/otrs.CheckModules.pl || return 1
+    logdebug "Adding system user..."
     useradd -d /opt/otrs/ -c 'OTRS user' otrs || return 1
     usermod -aG www-data otrs || return 1
+    logdebug "Activating default configuration files..."
     cp /opt/otrs/Kernel/Config.pm.dist /opt/otrs/Kernel/Config.pm || return 1
     cp /opt/otrs/Kernel/Config/GenericAgent.pm.dist /opt/otrs/Kernel/Config/GenericAgent.pm || return 1
+    logdebug "Checking whether all needed modules are installed..."
     perl -cw /opt/otrs/bin/cgi-bin/index.pl || return 1
     perl -cw /opt/otrs/bin/cgi-bin/customer.pl || return 1
     perl -cw /opt/otrs/bin/otrs.PostMaster.pl || return 1
     /opt/otrs/bin/otrs.SetPermissions.pl --otrs-user=otrs --web-user=www-data --otrs-group=www-data --web-group=www-data /opt/otrs || return 1
+    logdebug "Installing Apache httpd configuration file..."
     ln -s /opt/otrs/scripts/apache2-httpd.include.conf /etc/apache2/conf.d/otrs.config || return 1
     service apache2 restart || return 1
     executeMySQLQuery "create database $OTRS_DB_NAME charset utf8;" || return 1
@@ -54,9 +61,17 @@ function do_install {
     executeMySQLImport "$OTRS_DB_NAME" "/opt/otrs/scripts/database/otrs-schema-post.mysql.sql" || return 1
     executeMySQLQuery "GRANT ALL PRIVILEGES ON $OTRS_DB_NAME.* TO $OTRS_DB_USERNAME@localhost IDENTIFIED BY '$OTRS_DB_PASSWORD' WITH GRANT OPTION;" || return 1
     executeMySQLQuery "FLUSH PRIVILEGES;" || return 1
-    # FIXME update config file:
-    joe /opt/otrs/Kernel/Config.pm
+    logdebug "Changing standard configuration settings for database..."
+    cp /opt/otrs/Kernel/Config.pm /opt/otrs/Kernel/Config.pm.bak
+    sed -e \
+        "s/\$Self->{Database} = 'otrs';/\$Self->{Database} = '$OTRS_DB_NAME';/g" \
+        "s/\$Self->{DatabaseUser} = 'otrs';/\$Self->{DatabaseUser} = '$OTRS_DB_USERNAME';/g" \
+        "s/\$Self->{DatabasePw} = 'some-pass';/\$Self->{DatabasePw} = '$OTRS_DB_PASSWORD';/g" \
+        /opt/otrs/Kernel/Config.pm.bak > /opt/otrs/Kernel/Config.pm
     /opt/otrs/bin/otrs.CheckDB.pl || return 1
+    # TODO install cron jobs
+    # TODO configure mail system
+    # TODO configure scheduler (optional)
     
     loginfo "Installing OTRS-Extension-ReferenceIDoitObjects..."
     cd "$TMP_DIR" || return 1
@@ -69,7 +84,7 @@ function do_install {
         tar xzf "$tarball" || return 1
         /opt/otrs/bin/otrs.PackageManager.pl -a install -p ReferenceIDoitObjects-0.4/ReferenceIDoitObjects-0.4.opm || return 1
         service apache2 restart || return 1
-        # TODO configure extension, add dynamic fields
+        # TODO configure extension, add and configure dynamic fields
     fi
     
     cd "$BASE_DIR" || return 1
